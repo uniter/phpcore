59d3fa3d9aca67cf1a59745b3a965663bc110b86
Daniel Phillimore <dan@ovms.co>
2016-06-16 22:15:51 +0100

    Add basic integration test for PHP `try` statement

---
+++ test/integration/statements/tryTest.js
@@ -0,0 +1,42 @@
+/*
+ * PHPCore - PHP environment runtime components
+ * Copyright (c) Dan Phillimore (asmblah)
+ * https://github.com/uniter/phpcore/
+ *
+ * Released under the MIT license
+ * https://github.com/uniter/phpcore/raw/master/MIT-LICENSE.txt
+ */
+
+'use strict';
+
+var expect = require('chai').expect,
+    nowdoc = require('nowdoc'),
+    tools = require('../tools');
+
+describe('PHP "try" statement integration', function () {
+    it('should allow a thrown exception to be caught', function () {
+        var php = nowdoc(function () {/*<<<EOS
+<?php
+$result = [];
+
+class MyException extends Exception {}
+
+try {
+    $result[] = 1;
+    throw new MyException('Oh no');
+    $result[] = 2;
+} catch (MyException $ex1) {
+    $result[] = 3;
+} catch (NotMyException $ex2) {
+    $result[] = 4;
+}
+$result[] = 5;
+
+return $result;
+EOS
+*/;}),//jshint ignore:line
+            module = tools.syncTranspile(null, php);
+
+        expect(module().execute().getNative()).to.deep.equal([1, 3, 5]);
+    });
+});
