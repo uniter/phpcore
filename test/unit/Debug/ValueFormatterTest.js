a9e337f5428b3573dc1416b462c015111abf4e7a
Daniel Phillimore <dan@ovms.co>
2016-10-01 01:54:58 +0100

    Add tests for formatting of ObjectValues

--- test/unit/Debug/ValueFormatterTest.js
+++ test/unit/Debug/ValueFormatterTest.js
@@ -15,6 +15,7 @@ var expect = require('chai').expect,
     FloatValue = require('../../../src/Value/Float').sync(),
     IntegerValue = require('../../../src/Value/Integer').sync(),
     NullValue = require('../../../src/Value/Null').sync(),
+    ObjectValue = require('../../../src/Value/Object').sync(),
     StringValue = require('../../../src/Value/String').sync(),
     ValueFormatter = require('../../../src/Debug/ValueFormatter');
 
@@ -65,6 +66,59 @@ describe('ValueFormatter', function () {
             expect(this.formatter.format(value)).to.deep.equal({
                 style: 'font-weight: bold;',
                 displayValue: '<null>'
+            });
+        });
+
+        it('should display a non-function JSObject with constructor ObjectValue correctly', function () {
+            var MyClass = function MyClass() {},
+                myObject = new MyClass(),
+                value = sinon.createStubInstance(ObjectValue);
+            value.getClassName.returns('JSObject');
+            value.getNative.returns(myObject);
+            value.getType.returns('object');
+
+            expect(this.formatter.format(value)).to.deep.equal({
+                style: '',
+                displayValue: '<JS:MyClass>'
+            });
+        });
+
+        it('should display a non-function JSObject without constructor ObjectValue correctly', function () {
+            var MyClass = function MyClass() {},
+                myObject = new MyClass(),
+                value = sinon.createStubInstance(ObjectValue);
+            delete MyClass.prototype.constructor;
+            value.getClassName.returns('JSObject');
+            value.getNative.returns(myObject);
+            value.getType.returns('object');
+
+            expect(this.formatter.format(value)).to.deep.equal({
+                style: '',
+                displayValue: '<JS:Object>'
+            });
+        });
+
+        it('should display a function JSObject ObjectValue correctly', function () {
+            var value = sinon.createStubInstance(ObjectValue);
+            value.getClassName.returns('JSObject');
+            value.getNative.returns(function myFunc() {});
+            value.getType.returns('object');
+
+            expect(this.formatter.format(value)).to.deep.equal({
+                style: '',
+                displayValue: '<JS:function myFunc()>'
+            });
+        });
+
+        it('should display a non-JSObject ObjectValue correctly', function () {
+            var value = sinon.createStubInstance(ObjectValue);
+            value.getClassName.returns('My\\Space\\ThisIsMyClass');
+            value.getNative.returns({});
+            value.getType.returns('object');
+
+            expect(this.formatter.format(value)).to.deep.equal({
+                style: '',
+                displayValue: '<My\\Space\\ThisIsMyClass>'
             });
         });
 
