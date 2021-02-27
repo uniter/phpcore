b5ec84c9588268ae77525ea8b87a4c36cb414478
Dan Phillimore <dan@ovms.co>
2021-02-27 20:17:27 +0000

    Add test to ensure option factories have access to previous values for chaining

--- test/integration/ffi/api/builtin/options/syncModeTest.js
+++ test/integration/ffi/api/builtin/options/syncModeTest.js
@@ -42,4 +42,43 @@ EOS
 
         expect(module().execute().getNative()).to.equal(21);
     });
+
+    it('should give an option access to its previous value for chaining', function () {
+        var php = nowdoc(function () {/*<<<EOS
+<?php
+return get_my_option();
+EOS
+*/;}), //jshint ignore:line
+            runtime = tools.createSyncRuntime(),
+            module = tools.transpile(runtime, '/path/to/my_module.php', php);
+
+        runtime.configure({
+            'my_option': 'my initial value'
+        });
+        runtime.install({
+            optionGroups: [
+                function (internals) {
+                    var initialValue = internals.optionSet.getOption('my_option');
+
+                    return {
+                        'my_option': initialValue + ', my new value'
+                    };
+                }
+            ],
+
+            functionGroups: [
+                function (internals) {
+                    return {
+                        'get_my_option': function () {
+                            return internals.valueFactory.createInteger(
+                                internals.optionSet.getOption('my_option')
+                            );
+                        }
+                    };
+                }
+            ]
+        });
+
+        expect(module().execute().getNative()).to.equal('my initial value, my new value');
+    });
 });
