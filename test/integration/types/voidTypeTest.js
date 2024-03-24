3304bf7a03aebbe12cb0538795cf6023f694477d
Dan Phillimore <dan@ovms.co>
2024-03-24 20:48:55 +0000

    Add test for violation of void return type

--- test/integration/types/voidTypeTest.js
+++ test/integration/types/voidTypeTest.js
@@ -11,7 +11,9 @@
 
 var expect = require('chai').expect,
     nowdoc = require('nowdoc'),
-    tools = require('../tools');
+    phpCommon = require('phpcommon'),
+    tools = require('../tools'),
+    PHPFatalError = phpCommon.PHPFatalError;
 
 describe('PHP void return type integration', function () {
     it('should allow functions that have no explicit return or return void', async function () {
@@ -49,5 +51,28 @@ EOS
             'no explicit return': 21,
             'return void': 101
         });
+    });
+
+    it('should raise an error when a value is returned with void return type', async function () {
+        var php = nowdoc(function () {/*<<<EOS
+<?php
+
+// -- Some padding to inflate line numbers a bit --
+
+function myFunction(): void {
+    return 21;
+}
+
+myFunction();
+EOS
+*/;}); //jshint ignore:line
+
+        expect(function () {
+            // Void return violations are caught at transpile time.
+            tools.asyncTranspile('/path/to/my_module.php', php);
+        }).to.throw(
+            PHPFatalError,
+            'PHP Fatal error: A void function must not return a value in /path/to/my_module.php on line 6'
+        );
     });
 });
